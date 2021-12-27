6da790c0be1cb249d4d4dd3aa67be45c0816aa53
Matt Luker <jamesmattluker@gmail.com>
2021-12-26 19:26:10 -0700

    add Details view for clients

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -38,7 +38,11 @@ namespace HairSalon.Controllers
       return RedirectToAction("Index");
     }
 
-
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
   }
   
 }
